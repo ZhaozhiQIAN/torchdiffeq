5409011d64630618aad2a154f6b7a823376a8687
Zhaozhi Qian <qianzhaozhi@yahoo.com>
2019-12-11 20:06:18 +0000

    new simulation

--- examples/simulation_utils.py
+++ examples/simulation_utils.py
@@ -18,33 +18,35 @@ def get_data():
                              'niters', 'test_freq', 'viz', 'gpu', 'adjoint'])
     args = Arg('dopri5', 1000, 20, 1, 2000, 50, False, 1, True)
 
-    true_y0 = torch.tensor([[2.0, 1.0]], dtype=D_TYPE)
-    t = torch.linspace(0, 10, args.data_size, dtype=D_TYPE)
+    true_y0 = torch.tensor([[0.0, 5.0]])
+    t = torch.linspace(0, 5, args.data_size + 1)
 
     class Lambda(nn.Module):
-
-        def __init__(self, gamma=1., beta=0.5):
+        def __init__(self, w0, f0, w1, dr):
             super(Lambda, self).__init__()
-            self.gamma = gamma
-            self.beta = beta
+
+            self.w0 = torch.tensor(w0)
+            self.f0 = torch.tensor(f0)
+            self.w1 = torch.tensor(w1)
+            self.dr = torch.tensor(dr)
+
+        def force(self, t):
+            return self.f0 * torch.sin(self.w1 * t)
 
         def forward(self, t, y):
-            y1 = y[:, 0]
-            y2 = y[:, 1]
-            dy1_dt = y2
-            dy2_dt = - self.beta * y2 - self.gamma * y1
-
-            return torch.cat((dy1_dt.reshape(-1, 1), dy2_dt.reshape(-1, 1)), axis=1)
+            dy0_dt = y[:, 1]
+            dy1_dt = -2. * self.w0 * y[:, 1] * self.dr - self.w0 ** 2 * y[:, 0] + self.force(t)
+            return torch.cat((dy0_dt.reshape(-1, 1), dy1_dt.reshape(-1, 1)), axis=1)
 
     # This numerical solution given the true DE.
     with torch.no_grad():
-        true_y = odeint(Lambda(), true_y0, t, method='dopri5')
-    true_y = true_y[..., 0:1]
+        lam = Lambda(5., 5., 3., 0.01)
+        true_y = odeint(lam, true_y0, t, method='dopri5')
 
     dat_dict = dict()
 
     for s in range(args.data_size - args.batch_time):
-        batch_t = t[:args.batch_time]
+        batch_t = t[s:s+args.batch_time]
         batch_y = torch.stack([true_y[s + i] for i in range(args.batch_time)], dim=0)
         dim = batch_y.shape[-1]
         x_reshaped = batch_y.reshape(args.batch_time, 1, 1, dim)
--- examples/training_scripts.py
+++ examples/training_scripts.py
@@ -22,8 +22,10 @@ import GRUD
 
 parser = argparse.ArgumentParser('ODE Enc Dec')
 parser.add_argument('--mode', type=str, choices=['train', 'test'], default='train')
-parser.add_argument('--method', type=str, choices=['grud', 'ode'], default='ode')
+parser.add_argument('--method', type=str, choices=['grud', 'ode', 'attn'], default='ode')
+parser.add_argument('--encode_t', type=str, choices=['1', '2', '3'], default='1')
 parser.add_argument('--ode', type=str, choices=['standard', 'fs', 'ho', 'linear'], default='standard')
+parser.add_argument('--ode_solver', type=str, choices=['adjoint', 'dto'], default='adjoint')
 parser.add_argument('--fresh_train', type=str, choices=['yes', 'no'], default='yes')
 parser.add_argument('--gpu', type=str, choices=['0', '1'], default='0')
 args = parser.parse_args()
@@ -31,7 +33,7 @@ DEVICE = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 
 
 
 class GrudEncDec():
-    def run(self, mode, fresh_train):
+    def run(self, mode, fresh_train, encode_t):
         # data
         dat_dict = training_utils.get_data('data/cprd_sample_5_obs_6yr.csv.gz', normalize=False)
         dat_folds = training_utils.get_fold(dat_dict, fold=5, seed=666)
@@ -46,7 +48,7 @@ class GrudEncDec():
         test_freq = 100
         data_fold = dat_folds[0]
         batch_size = 100
-        encode_t = 1.
+        encode_t = float(encode_t)
 
         # models
         gru = GRUD.GRUD_V2(input_size=dim_y, hidden_size=50, output_last=True, device=DEVICE)
@@ -98,14 +100,15 @@ class GrudEncDec():
 
 
 class OdeDecGrudEnc():
-    def run(self, mode, fresh_train='yes', ode_type='standard'):
+    def run(self, mode, fresh_train='yes', ode_type='standard', ode_solver='adjoint', encode_t='1'):
         print('Training OdeDecGrudEnc')
         # data
         dat_dict = training_utils.get_data('data/cprd_sample_5_obs_6yr.csv.gz', normalize=False)
         dat_folds = training_utils.get_fold(dat_dict, fold=5, seed=666)
 
         # parameters
-        model_path = 'exp2-models/ode_dec_grud_enc_' + ode_type + '_{}.pth'
+        spec_string = '{}_{}_{}'.format(ode_type, ode_solver, encode_t)
+        model_path = 'exp2-models/ode_dec_grud_enc_' + spec_string + '_{}.pth'
         dim_y = 9
         dim_hidden = 50
         dim_decoder_mlp_hidden = 50
@@ -114,7 +117,7 @@ class OdeDecGrudEnc():
         test_freq = 50
         data_fold = dat_folds[0]
         batch_size = 100
-        encode_t = 1.
+        encode_t = float(encode_t)
 
         # models
         gru = GRUD.GRUD_V2(input_size=dim_y, hidden_size=50, output_last=True, device=DEVICE)
@@ -135,7 +138,12 @@ class OdeDecGrudEnc():
                 ode_func.reset_counter()
             init_cond = gru(gru_d_input)[:, None, :]
             t_dec = torch.cumsum(t_delta_dec, 0)
-            y_pred = odeint(ode_func, init_cond, t_dec)
+            if ode_solver == 'adjoint':
+                y_pred = odeint(ode_func, init_cond, t_dec)
+            elif ode_solver == 'dto':
+                y_pred = dto(ode_func, init_cond, t_dec)
+            else:
+                raise ValueError('Incorrect ode solver')
             if mode == 'train':
                 ode_func.reset_counter()
             y_out = decoder(y_pred.squeeze())
@@ -180,10 +188,93 @@ class OdeDecGrudEnc():
         return out
 
 
+class OdeDecAttn():
+    def run(self, mode, fresh_train='yes', ode_type='standard', encode_t='1'):
+        print('{} OdeDecAttn'.format(mode))
+        # data
+        dat_dict = training_utils.get_data('data/cprd_sample_5_obs_6yr.csv.gz', normalize=False)
+        dat_folds = training_utils.get_fold(dat_dict, fold=5, seed=666)
+
+        # parameters
+        spec_string = '{}_{}'.format(ode_type, encode_t)
+        model_path = 'exp2-models/ode_dec_attn_' + spec_string + '_{}.pth'
+        dim_y = 9
+        dim_hidden = 50
+        dim_hidden_ode = 50 if ode_type == 'standard' else None
+        dim_decoder_mlp_hidden = 50
+
+        niters = 2000
+        test_freq = 50
+        data_fold = dat_folds[0]
+        batch_size = 100
+        encode_t = float(encode_t)
+
+        # models
+        gru = GRUD.GRUD_V2(input_size=dim_y, hidden_size=50, output_last=False, device=DEVICE)
+        ode_func = ode_models.AttentiveODE(dim_hidden, dim_hidden_ode).to(DEVICE)
+        decoder = baseline_models.SLP(dim_hidden, dim_y, dim_decoder_mlp_hidden).to(DEVICE)
+
+        def ode_dec_attn_loss_func(y_target, y_target_mask, gru_d_input, t_delta_dec, mode='train'):
+            if mode == 'train':
+                ode_func.reset_counter()
+
+            gru_hidden = gru(gru_d_input)
+            init_cond = gru_hidden[:, -2:-1, :]
+            t_dec = torch.cumsum(t_delta_dec, 0)
+
+            ode_func.set_gru_hidden(gru_hidden)
+            y_pred = dto(ode_func, init_cond, t_dec)
+
+            if mode == 'train':
+                ode_func.reset_counter()
+            y_out = decoder(y_pred.squeeze())
+            loss = torch.mean(torch.abs(y_out[y_target_mask] - y_target[y_target_mask]))
+            return loss
+
+        def ode_dec_attn_save_func():
+            torch.save(gru.state_dict(), model_path.format('encoder'))
+            torch.save(ode_func.state_dict(), model_path.format('ode_func'))
+            torch.save(decoder.state_dict(), model_path.format('decoder'))
+            with open('exp2-models/ode_dec_attn_' + ode_type + '_counter.pkl', 'wb') as f:
+                pickle.dump(ode_func.counter_list, f)
+
+        def ode_dec_attn_load_func():
+            gru.load_state_dict(torch.load(model_path.format('encoder')))
+            gru.eval()
+            ode_func.load_state_dict(torch.load(model_path.format('ode_func')))
+            ode_func.eval()
+            decoder.load_state_dict(torch.load(model_path.format('decoder')))
+            decoder.eval()
+
+        if fresh_train != 'yes' or mode != 'train':
+            ode_dec_attn_load_func()
+
+        optimizer = optim.Adam(list(gru.parameters()) +
+                               list(ode_func.parameters()) +
+                               list(decoder.parameters()), lr=1e-3)
+
+        if mode == 'train':
+            out = training_utils.training_loop_v2(niters,
+                                                  data_fold,
+                                                  batch_size,
+                                                  encode_t,
+                                                  optimizer,
+                                                  test_freq,
+                                                  ode_dec_attn_loss_func,
+                                                  ode_dec_attn_save_func,
+                                                  DEVICE)
+        else:
+            out = training_utils.testing(data_fold, encode_t, ode_dec_attn_loss_func, DEVICE)
+        print(out)
+        return out
+
+
 if __name__ == '__main__':
     if args.method == 'grud':
-        GrudEncDec().run(args.mode, args.fresh_train)
+        GrudEncDec().run(args.mode, args.fresh_train, args.encode_t)
     elif args.method == 'ode':
-        OdeDecGrudEnc().run(args.mode, args.fresh_train, args.ode)
+        OdeDecGrudEnc().run(args.mode, args.fresh_train, args.ode, args.ode_solver, args.encode_t)
+    elif args.method == 'attn':
+        OdeDecAttn().run(args.mode, args.fresh_train, args.ode, args.encode_t)
     else:
         raise ValueError
